4f422b6de06866437c4df0c53cee6e3aea86b6e1
yusuf <myusuf@clavax.com>
2023-07-25 19:09:13 +0530

    setup projects

---
+++ mmq/mmq/setting/local.py
@@ -0,0 +1,23 @@
+from .base import *
+
+DOMAIN_NAME = 'http://127.0.0.1:8000'
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost:4200', '*']
+
+
+# DATABASES = {
+#     'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'simplypy_local',
+#             'USER': 'simplyfy',
+#             'PASSWORD': 'tech',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#             },
+# }
+
+API_URL = "http://127.0.0.1:8000/api/v1/"
+# FILE_PATH = "/var/www/html/simplyfy/Simplyfy/simplyfy"
+# NEW_API_URL = "http://127.0.0.1:8000/api/v1/"
+
